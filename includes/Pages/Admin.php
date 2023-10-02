08f9fd1c017a04551e7c5b4ffaa812bc2ee2dc85
Vivian Hayes <vivian.xw7@gmail.com>
2023-10-01 20:15:42 -0500

    re-add CPT's

--- includes/Pages/Admin.php
+++ includes/Pages/Admin.php
@@ -6,6 +6,12 @@ namespace Includes\Pages;
 class Admin {
     public function initialize() {
         add_action( 'admin_menu', array($this, 'add_admin_pages') );
+
+        add_action('init', array($this, 'register_custom_investments_cpt'));
+            
+        add_action('init', array($this, 'register_custom_operating_experience_cpt'));
+        
+        add_action('init', array($this, 'register_custom_taxonomy'));
     }
 
     public function add_admin_pages ()
@@ -16,9 +22,69 @@ class Admin {
     public function admin_index()
     {
         // require template
-        // require_once( plugin_dir_path( __FILE__ ) . 'templates/admin.php' );
         require_once( PLUGIN_PATH . 'templates/archive-gmp.php' );
     }
+
+    // Investment CPT
+    public function register_custom_investments_cpt() {
+        $labels = array(
+            'name' => 'Investments',
+            'singular_name' => 'Investment',
+            'menu_name' => 'Investments',
+            'add_new' => 'Add New',
+            'add_new_item' => 'Add New Investment',
+        );
+
+        $args = array(
+            'labels' => $labels,
+            'public' => true,
+            'has_archive' => true,
+            'publicly_queryable' => true,
+            'query_var' => true,
+            'rewrite' => array('slug' => 'investments'),
+            'menu_icon' => 'dashicons-chart-bar',
+            'supports' => array('title', 'editor', 'thumbnail'),
+        );
+
+        register_post_type('investment', $args);
+    }
+    
+    public function register_custom_taxonomy() {
+        $labels = array(
+            'name' => 'Investment Categories',
+            'singular_name' => 'Investment Category',
+        );
+
+        $args = array(
+            'labels' => $labels,
+            'public' => true,
+            'hierarchical' => true,
+        );
+
+        register_taxonomy('investment_category', 'investment', $args);
+    }
+
+    // Operating Experience CPT
+    public function register_custom_operating_experience_cpt() {
+        $labels = array(
+            'name' => 'Operating Experience',
+            'singular_name' => 'Operating Experience',
+            'menu_name' => 'Operating Experience',
+            'add_new' => 'Add New',
+            'add_new_item' => 'Add New Operating Experience',
+        );
+
+        $args = array(
+            'labels' => $labels,
+            'public' => true,
+            'has_archive' => true,
+            'publicly_queryable' => true,
+            'query_var' => true,
+            'rewrite' => array('slug' => 'operating-experience'),
+            'menu_icon' => 'dashicons-businessman',
+            'supports' => array('title', 'editor', 'thumbnail'),
+        );
+
+        register_post_type('operating_experience', $args);
+    }
 }
-
-
